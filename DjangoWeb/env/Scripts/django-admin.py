bfc1d283553550cd7866648f2b494bf9b74cea8b
daniel mandarano <danielmandarano@yahoo.com.ar>
2018-10-27 17:22:16 -0300

    Commit 2

---
+++ DjangoWeb/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\daniel\Documents\DjangoVisualStudio\DjangoWeb\DjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
